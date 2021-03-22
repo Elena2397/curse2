5e0e66a742f2703ec727e0ce21b4758200b3b690
Elena <shatilova7elena@mail.ru>
2021-03-22 14:16:23 +0300

    Lesson 41

--- Food/js/script.js
+++ Food/js/script.js
@@ -38,4 +38,50 @@ window.addEventListener('DOMContentLoaded', function() {
             });
 		}
 	});
+    // Timer
+    const deadline = '2021-05-11';
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t/(1000*60*60*24)),
+              hours = Math.floor((t / (1000*60*60)%24)),
+              minutes =Math.floor((t/1000/60)%60),
+              second = Math.floor((t /1000)%60);
+        return{
+            'total':t,
+            'days':days,
+            'hours': hours,
+            'minutes': minutes,
+            'second': second
+        };
+    }
+
+    function getZero(num){
+        if(num >= 0 && num < 10 ){
+            return `0${num}`;
+        } else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            second = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        updateClock();
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            second.innerHTML = getZero(t.second);
+
+            if(t.total <=0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', deadline);
 });
