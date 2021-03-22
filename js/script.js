33907741d777a42ae36ffa32228580975c685590
Elena <shatilova7elena@mail.ru>
2021-03-22 10:44:14 +0300

    practicy(32)

--- js/script.js
+++ js/script.js
@@ -1,42 +1,23 @@
 "use strict";
+    const btn = document.querySelector('button');
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart'),
-      wrapper =  document.querySelector('.wrapper');
+    // btn.onclick = function(){
+    //     alert('click');
+    // }
 
-//box.style.backgroundColor = 'blue';
-//box.style.width = '500px';
+    // btn.addEventListener('click', () => {
+    //     alert('click');
+    // });
 
-box.style.cssText = 'background-color: blue; width: 500px';
+    let i = 0;
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
+    const deleteElement = (e) => {
+        console.log(e.target);
+        i++;
+        if(i == 1){
+            btn.removeEventListener('click', deleteElement);
+        }
+    };
 
-//    for(let i = 0; i < hearts.length; i++ ){
-//        hearts[i].style.backgroundColor = 'blue';
-//    }
-
-hearts.forEach(item =>{
-    item.style.backgroundColor = 'blue';
-});
-
-const div = document.createElement('div');
-//const text = document.createTextNode('тут быд я');
-div.classList.add('black');
-//wrapper.append(div);
-wrapper.prepend(div);
-
-//hearts[0].before(div);
-
-//hearts[0].after(div);
-
-//circles[0].remove();
-
-//hearts[0].replaceWith(circles[0]);
- 
-
-//div.innerHTML = "<h1>Hello World</h1>";
-div.insertAdjacentHTML("beforebegin",'<h2>Hello</h2>');
+    btn.addEventListener('click',deleteElement);
+    // btn.removeEventListener('click',deleteElement);
